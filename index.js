423988c07966fdf5ddddad1a35999631b8b71fc7
root <root@ip-172-31-47-201.ap-northeast-1.compute.internal>
2018-12-05 14:24:39 +0000

    Improve

--- index.js
+++ index.js
@@ -29,12 +29,21 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
     req.body.events.forEach((event) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         if (event.type == "message" && event.message.type == "text"){
-            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-            if (event.message.text == "こんにちは"){
+            // event.message.test　→　受信メッセージ
+            // 曲を選択
+            if (event.message.text == "曲を選択"){
                 // replyMessage()で返信し、そのプロミスをevents_processedに追加。
                 events_processed.push(bot.replyMessage(event.replyToken, {
                     type: "text",
-                    text: "これはこれは"
+                    text: "曲を選択してください。"
+                }));
+            }
+            // パートを選択
+            if (event.message.text == "パートを選択"){
+                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: "パートを選択してください。"
                 }));
             }
         }
