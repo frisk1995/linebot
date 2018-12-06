015a2ce88ef541d31782ff65d2ff86ebb028c8ce
frisk1995 <toshiki.priv@gmail.com>
2018-12-06 22:27:21 +0900

    add

---
+++ createdb.sql
@@ -0,0 +1,28 @@
+create database part;
+create database title;
+
+create table part(
+partId int(5) auto_increment primary key,
+part varchar(255) not null
+);
+
+desc part;
+
+create table music(
+titleId int(5) auto_increment primary key,
+title varchar(255) not null
+);
+
+desc music;
+
+create table main(
+id int(5) auto_increment primary key,
+date datetime,
+titleId int(5) not null,
+name varchar(255) not null,
+partId int(5) not null,
+foreign key(titleId) references music(titleId),
+foreign key(partId) references music(partId)
+);
+
+desc main;
