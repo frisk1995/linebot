e1fae2d14007031455842cd6edb830ed395eaf91
frisk1995 <toshiki.priv@gmail.com>
2018-12-07 17:25:19 +0900

    Update createdb.sql

--- createdb.sql
+++ createdb.sql
@@ -40,10 +40,10 @@ desc attend;
 insert into part(part) values('ボーカル'),('ギター'),('ベース'),('ドラム'),('キーボード');
 
 insert into music(title,artist) values('チェリー','スピッツ'),('リライト','アジカン'),('風吹けば恋','チャットモンチー'),('シルエット','KANA-BOON'),('アボカド','yonige'),('ファンファーレ','sumika'),('真赤','My hair is bad');
+
 insert into event(eventdate, place) values('2018年12月22日','スタジオノア-秋葉原店');
 
 insert into attend(eventId,titleId,name,partId) values('1',4,'まさる',1);
 insert into attend(eventId,titleId,name,partId) values('1',7,'とっしー',2);
 
-
-
+select id,date,music.title,name from attend join music on attend.titleId = music.titleId;
